0adf0ee610b0d93d4acf248b38104685f3f83c58
Can <enmuhammet23@gmail.com>
2020-10-05 21:48:55 +0300

    Added HTTP service

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,3 @@
 class globals {
-  static const String URL = "http://192.168.43.203:8080/";
+  static const String URL = "http://35.214.92.199:8080/";
 }
--- lib/view/register/register_view.dart
+++ lib/view/register/register_view.dart
@@ -1,125 +1,122 @@
-import 'dart:convert';
-
 import 'package:MoneyTracker/locator.dart';
 import 'package:MoneyTracker/routing/resources/images.dart';
 import 'package:MoneyTracker/routing/resources/styles.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:stacked/stacked.dart';
-import 'package:MoneyTracker/globals.dart';
 
 class registerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ViewModelBuilder<RegisterViewModel>.nonReactive(
-        viewModelBuilder: () => RegisterViewModel(),
-        builder: (context, model, child) => Container(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 50),
-            Flexible(
-              child: Hero(
+      body: SingleChildScrollView(
+        child: ViewModelBuilder<RegisterViewModel>.nonReactive(
+          viewModelBuilder: () => RegisterViewModel(),
+          builder: (context, model, child) => Container(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50),
+              Hero(
                 tag: 'app-icon',
                 child: Container(
                   height: 150,
                   child: Image.asset(Images.icon),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Form(
-                child: Column(
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        style: TextStyles.input,
+                        onChanged: (value) => model.name = value,
+                        decoration: Decorations.input
+                            .copyWith(labelText: 'Name Surname'),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyles.input,
+                        onChanged: (value) => model.email = value,
+                        decoration:
+                            Decorations.input.copyWith(labelText: 'E-mail'),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        style: TextStyles.input,
+                        onChanged: (value) => model.username = value,
+                        decoration:
+                            Decorations.input.copyWith(labelText: 'Username'),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        style: TextStyles.input,
+                        onChanged: (value) => model.password = value,
+                        obscureText: true,
+                        decoration: Decorations.input.copyWith(
+                            labelText: 'Password',
+                            contentPadding: EdgeInsets.zero),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        style: TextStyles.input,
+                        obscureText: true,
+                        onChanged: (value) => model.rePassword = value,
+                        decoration: Decorations.input.copyWith(
+                            labelText: 'Re-enter Password',
+                            contentPadding: EdgeInsets.zero),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                    // side:BorderSide(color: Colors.blue)
+                  ),
+                  onPressed: model.sendForm,
+                  color: Colors.red,
+                  child: Text(
+                    'Register',
+                    style: TextStyles.button,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  style: TextStyles.info,
                   children: [
-                    TextFormField(
-                      style: TextStyles.input,
-                      onChanged: (value) => model.name = value,
-                      decoration:
-                          Decorations.input.copyWith(labelText: 'Name Surname'),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyles.input,
-                      onChanged: (value) => model.email = value,
-                      decoration:
-                          Decorations.input.copyWith(labelText: 'E-mail'),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      style: TextStyles.input,
-                      onChanged: (value) => model.username = value,
-                      decoration:
-                          Decorations.input.copyWith(labelText: 'Username'),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      style: TextStyles.input,
-                      onChanged: (value) => model.password = value,
-                      obscureText: true,
-                      decoration: Decorations.input.copyWith(
-                          labelText: 'Password',
-                          contentPadding: EdgeInsets.zero),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      style: TextStyles.input,
-                      obscureText: true,
-                      onChanged: (value) => model.rePassword = value,
-                      decoration: Decorations.input.copyWith(
-                          labelText: 'Re-enter Password',
-                          contentPadding: EdgeInsets.zero),
-                    ),
+                    TextSpan(text: 'If you have already an account'),
+                    const WidgetSpan(child: SizedBox(width: 4)),
+                    TextSpan(
+                      text: 'Go To Login',
+                      style: TextStyles.infoClickable,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = model.navigateLogin,
+                    )
                   ],
                 ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-              child: MaterialButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18),
-                  // side:BorderSide(color: Colors.blue)
-                ),
-                onPressed: model.sendForm,
-                color: Colors.red,
-                child: Text(
-                  'Register',
-                  style: TextStyles.button,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            RichText(
-              text: TextSpan(
-                style: TextStyles.info,
-                children: [
-                  TextSpan(text: 'If you have already an account'),
-                  const WidgetSpan(child: SizedBox(width: 4)),
-                  TextSpan(
-                    text: 'Go To Login',
-                    style: TextStyles.infoClickable,
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = model.navigateLogin,
-                  )
-                ],
-              ),
-            ),
-          ],
-        )),
+            ],
+          )),
+        ),
       ),
     );
   }
@@ -137,14 +134,14 @@ class RegisterViewModel extends ChangeNotifier {
       print("Error");
       return;
     }
-    Response res = await post(globals.URL + 'user/signup',
-        headers: {'content-type': 'application/json'},
-        body: jsonEncode({
-          'name': this.name,
-          'email': this.email,
-          'password': this.password,
-          'username': this.username
-        }));
+    Map<String, String> formData = {
+      'name': this.name,
+      'email': this.email,
+      'password': this.password,
+      'username': this.username
+    };
+    var res = await httpService.postRequest('user/signup', body: formData);
+    print(res['message']);
   }
 
   void navigateLogin() {
